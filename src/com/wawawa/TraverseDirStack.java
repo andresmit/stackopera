ad563b72edf3116df3c43cb876a90476a045b443
andresmatsin <andres.matsin@wazombi.com>
2016-10-06 11:13:54 +0300

    done

--- src/com/wawawa/TraverseDirStack.java
+++ src/com/wawawa/TraverseDirStack.java
@@ -1,11 +1,11 @@
-package com.wazombi;
+package com.wawawa;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Stack;
 
-public class Main {
+public class TraverseDirStack {
 
 
     public static void main(String[] args) {
@@ -15,29 +15,47 @@ public class Main {
 
     static void dirTraverse(String path){
         Stack<File> stack = new Stack<>();
+        Stack<Integer> taane = new Stack<>();
+
 
         File[] files = new File(path).listFiles();
 
         for (File ff: files){
+            taane.add(0);
             stack.add(ff);
         }
         ArrayList<File> fs = new ArrayList(Arrays.asList(new File(path).listFiles()));
 
         while (!stack.empty()) {
             File f = stack.pop();
+            int taanePraegu = taane.pop();
+
             if (f.isDirectory()){
 
-                System.out.println("[" + f.getName() + "]");
+                System.out.println( ehitaTaane(taanePraegu) + "[" + f.getName() + "]");
+
+                int taaneUus = taanePraegu + 1;
+
                 for (File fil:f.listFiles()){
+                    taane.add(taaneUus);
                     stack.add(fil);
                 }
+
             } else {
                 if (f.length() < 500 * 1000) {
-                    System.out.print(f.getName() + " ");
+                    System.out.print(ehitaTaane(taanePraegu)+ f.getName() + " ");
                     System.out.println(f.length());
                 }
             }
         }
+    }
+
+    static String ehitaTaane(int n){
+        StringBuilder s = new StringBuilder("");
+        for (int i = 0; i < n; i++) {
+            s.append("   ");
+        }
+        return new String(s);
     }
 
 
